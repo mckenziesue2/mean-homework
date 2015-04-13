4d4a9cff3f0302b56a4657ab6eae9c0d2aa2e5d6
McKenzie Rucker <mrucker@verisagellc.com>
2015-04-13 10:05:08 -0600

    homework finish

---
+++ homework1-calculator.js
@@ -0,0 +1,37 @@
+var ask = require('readline-sync');
+
+console.log('Write an equation! Use +, -, /, or * with any digits.');
+
+var equation = ask.question('What is your equation?');
+
+var noSpaces = equation.replace(' ', '');
+
+var divisionLocation = equation.indexOf("/");
+var additionLocation = equation.indexOf("+");
+var subtractionLocation = equation.indexOf("-");
+var multiplicationLocation = equation.indexOf("*");
+
+if (divisionLocation != -1) {
+  var mathLocation = divisionLocation;
+} else if (multiplicationLocation != -1) {
+  var mathLocation = multiplicationLocation;
+} else if (subtractionLocation != -1) {
+  var mathLocation = subtractionLocation;
+} else {
+  var mathLocation = additionLocation;
+}
+
+var firstNumber = equation.substr(0, mathLocation);
+var nextNumber = equation.substr(mathLocation + 1);
+
+if (divisionLocation != -1) {
+  var answer = firstNumber /= nextNumber;
+} else if (multiplicationLocation != -1) {
+  var answer = firstNumber *= nextNumber;
+} else if (subtractionLocation != -1) {
+  var answer = firstNumber -= nextNumber;
+} else {
+  var answer = firstNumber += nextNumber;
+}
+
+console.log(answer);
---
+++ homework1-rock-paper-scissors.js
@@ -0,0 +1,47 @@
+var ask = require('readline-sync');
+
+var myChoice = ask.question('Do you choose rock, paper, or scissors?');
+
+var myChoiceLower = myChoice.toLowerCase();
+
+console.log(myChoiceLower);
+
+var computerChoice = Math.random();
+
+if (0 < computerChoice < 0.33) {
+	computerChoice = "rock";
+} else if (0.33 < computerChoice < 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+}
+
+var computerChoiceLower = computerChoice.toLowerCase();
+
+console.log(computerChoiceLower);
+
+var compare = function(choice1, choice2) {
+	if (choice1 === choice2) {
+		return "It is a tie!";
+	} else if (choice1 === "rock") {
+		if (choice2 === "paper") {
+			return "Paper wins!";
+		} else {
+			return "Rock wins!";
+		}
+	} else if (choice1 === "rock") {
+		if (choice2 === "scissors") {
+			return "Scissors wins!";
+		} else {
+			return "Paper wins!";
+		}
+	} else if (choice1 === "scissors") {
+		if (choice2 === "rock") {
+			return "Rock wins!";
+		} else {
+			return "Scissors wins!";
+		}
+	}
+};
+
+console.log(compare(myChoiceLower, computerChoiceLower));
