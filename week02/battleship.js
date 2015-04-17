9a13971384bf5e22339800b1a60fa4b1955c6310
McKenzie Rucker <mrucker@verisagellc.com>
2015-04-16 19:11:16 -0600

    changes

--- week02/battleship.js
+++ week02/battleship.js
@@ -1,11 +1,3 @@
-var ask = require('readline-sync');
-
-var myChoice = ask.question('Choose your coordinates: (-,-).');
-
-var grid = [];
-var rows = 10;
-var columns = 10;
-
 var location = {
 	var shipLocation = Math.random() * 5;
 	var ship = Math.floor(shipLocation);
@@ -19,21 +11,24 @@ var location = {
 		}
 };
 
+var grid = [];
 
 var iArray = [0,1,2,3,4,5,6,7,8,9];
 var jArray = [0,1,2,3,4,5,6,7,8,9];
-var gridLine = [];
 
 for (var i = 0; i < iArray.length; i++) {
 	var printGrid = [];
 
 	for (var j = 0; j < jArray.length; j++) {
-		printGrid.push( + i +',' + j );
+		printGrid.push( i +',' + j );
 	}
-	gridLine.push(printGrid);
-	printGrid = [];
+	grid.push(printGrid);
 }
 
-for (var i = 0; i < gridLine.length; i++) {
-	console.log(gridLine.replace([i], "0");
+for (var i = 0; i < grid.length; i++) {
+	console.log(grid[i]);
 }
+
+// var ask = require('readline-sync');
+
+// var myChoice = ask.question('Choose your coordinates: (-,-).');
