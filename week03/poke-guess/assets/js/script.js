8b84612150ad33a7f8a4897787e04099e3969474
McKenzie Rucker <mrucker@verisagellc.com>
2015-05-05 18:24:14 -0600

    catching up on poke guess

--- week03/poke-guess/assets/js/script.js
+++ week03/poke-guess/assets/js/script.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
 
-	//Declare Pokemon object class, Pokemon object instances
-	var Pokemon = function(name,image){
-		name: this.name;
-		image: "assets/img/" + this.image + ".png";
-	};
-
-	var score = 0;
-	var incorrect = 0;
+	//Declare Pokemon object class
+	function Pokemon(name,image){
+		this.name = name;
+		this.image = "assets/img/" + image + ".png";
+	}
 
 	var p1 = new Pokemon('bulbasaur','001');
 	var p2 = new Pokemon('ivysaur','002');
@@ -30,31 +27,33 @@ $(document).ready(function() {
 	var p19 = new Pokemon("rattata", "019");
 	var p20 = new Pokemon("raticate", "020");
 
-	//Initialize guess variables, arrays for unguessed (unguessed/guessed-wrong) and (guess-right) Pokemon
+	var score = 0;
+	var incorrect = 0;
+
 	var unguessedPokemon = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20];
 	var guessedPokemon = [];
 
 
-	//Declare variables and function for randomizing pokemon and accessing it
 	var randomPokemon;
+	var randomPokemonGenerator;
 	
 	var getRandomPokemon = function() {
-		randomPokemonGenerator = parseInt(Math.floor(Math.random()*5));
+		randomPokemonGenerator = parseInt(Math.floor(Math.random()*unguessedPokemon.length));
 		return unguessedPokemon[randomPokemonGenerator];
 	}
 
-	//Declare function to change images of guess pokemon and pokeballs
+	//Function to change images of guess pokemon and pokeballs
 	function changeImage(currentImgID, newImg) {
 		$(currentImgID).attr("src", newImg);
 	}
 
-	//Declare function to change pokemon
+	//Function to change pokemon
 	function changePokemon() {
 		randomPokemon = getRandomPokemon();
 		changeImage("#pokemon-img", randomPokemon.image);
 	}
 
-	//Declare function to show images of sad pokemon
+	//Function to show images of sad pokemon
 	function showSad() {
 		$("#sad" + incorrect).show();
 	}
@@ -65,7 +64,7 @@ $(document).ready(function() {
 	var winGameSound = new Audio("assets/audio/win_music.wav");
 	var gameOverSound = new Audio("assets/audio/game_over.wav");
 
-	//Declare timer function and initialize it at 10 seconds
+	//Timer function and start it at 10 seconds
 	var timerNum = 10;
 
 	function setTimer() {
@@ -84,7 +83,7 @@ $(document).ready(function() {
 			incorrect += 1;
 			showSad();
 			incorrectSound.play();
-			alert("Times up!");
+			alert("Times up! It was " + (randomPokemon.name).toUpperCase());
 
 			if (incorrect === 3) {
 				gameOverSound.play();
@@ -94,19 +93,20 @@ $(document).ready(function() {
 
 			changePokemon();
 
-			$("user-guess").val("");
+			$("#user-guess").val("");
 			timerNum = 10;
 		}
 	}
 
-	//Declare function to check guesses and return result
+	//Function to check guesses and return result
 	function checkGuess() {
 		var input = $("#user-guess").val();
 		var guess = input.toLowerCase();
 
 		if (guess === randomPokemon.name) { //guessed right
 			score += 1;
-			var guess = input.toLowerCase();
+			changeImage("#ball" + score, "assets/img/pokeball.png");
+			correctSound.play();
 
 			if (score === 5) {
 				winMusic.play();
@@ -120,10 +120,11 @@ $(document).ready(function() {
 			incorrect += 1;
 			showSad();
 			incorrectSound.play();
-			alert("Sorry! The correct answer is: " + (randomPokemonGenerator.name).toUpperCase());
+			alert("Sorry! The correct answer is " + (randomPokemon.name).toUpperCase());
 
 			if (incorrect === 3) {
 				gameOverSound.play();
+				alert("GAME OVER");
 				location.reload();
 			}
 		}
@@ -133,7 +134,7 @@ $(document).ready(function() {
 		timerNum = 10;
 	}
 
-	//Intialize conditions for the page
+	//Beginning page
 	$('#sad1, #sad2, #sad3').hide();
 	changePokemon();
 
